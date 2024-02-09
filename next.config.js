6f36d5f1069498a5e67bcc5676d68b359920789a
Bruno Vitte <bruno_vitte@hotmail.com>
2024-02-09 08:30:10 -0600

    change workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-/** @type {import('next').NextConfig} */
-/* uncomment for development 
- * const nextConfig = {}
- * module.exports = nextConfig
- */
-
 /**
  * @type {import('next').NextConfig}
  */
